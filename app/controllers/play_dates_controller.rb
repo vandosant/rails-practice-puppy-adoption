64fddb2fe09f3a0da150b4a1d103218a9fbbac47
vandosant <sjskender@gmail.com>
2014-08-06 13:39:05 -0600

    Styling with CSS

--- app/controllers/play_dates_controller.rb
+++ app/controllers/play_dates_controller.rb
@@ -1,7 +1,7 @@
 class PlayDatesController < ApplicationController
   def new
     @puppy = Puppy.find(params[:puppy_id])
-    @play_date = PlayDate.new
+    @play_date = PlayDate.new(date: Time.now)
   end
 
   def create
